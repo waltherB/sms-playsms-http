2f002348f2d5af1df99d7586d225cb19acf14651
Walther Barnett <>
2024-08-01 12:34:01 +0200

    change to github

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
     'name': "PlaySMS Connector",
     'summary': "Send SMS with PlaySMS",
     'author': "Walther Barnett",
-    'website': "https://gundogpro.dk",
+    'website': "https://github.com/waltherB/sms-playsms-http",
     'license': 'AGPL-3',
     'category': 'Technical',
     'version': '17.0.1.0.0',
