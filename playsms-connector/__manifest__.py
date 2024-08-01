713ebadac1d7bd688fd1e5da19fdf6d43858930f
Walther Barnett <>
2024-08-01 13:37:51 +0200

    add pngs

--- playsms-connector/__manifest__.py
+++ playsms-connector/__manifest__.py
@@ -18,5 +18,5 @@
         'views/sms_sms.xml',
         'views/sms_resend.xml'
     ],
-    'images': ['static/description/playsms.png'],
+    'images': ['static/description/playsms.png','static/description/banner.png'],
 }
